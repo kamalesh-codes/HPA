8aa8f71de6973539fe389a063114e432d27d5094
kamalesh <215050157+kamalesh-codes@users.noreply.github.com.>
2026-07-24 23:24:35 +0530

    fix

--- model.py
+++ model.py
@@ -1,14 +1,18 @@
 import torch.nn as nn
-from torchvision.models import resnet34
+from torchvision.models import densenet121
 from omegaconf import DictConfig
 
 
 def get_model(cfg:DictConfig):
 
-    model = resnet34()
-    model.conv1 = nn.Conv2d(4, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-    model.fc = nn.Sequential(
-                        nn.Linear(in_features=model.fc.in_features,out_features=128),
+
+    model = densenet121()
+    model.features[0]=nn.Conv2d(4, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+    model.classifier = nn.Sequential(
+                        nn.Linear(in_features=model.classifier.in_features,out_features=512),
+                        nn.ReLU(),
+                        nn.BatchNorm1d(num_features=512),
+                        nn.Linear(in_features=512,out_features=128),
                         nn.ReLU(),
                         nn.BatchNorm1d(num_features=128),
                         nn.Linear(in_features=128,out_features=cfg.data.num_class))
